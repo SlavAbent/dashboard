501bd104e98ce193b3ca9b86291b4397fcd4227f
astoe1 <abentslava@gmail.com>
2023-05-09 18:57:02 +0500

    иконка

---
+++ src/stories/UI/Components/Icon/model/Icon.model.ts
@@ -0,0 +1,10 @@
+import { DOMAttributes } from 'react'
+
+export type IconType = 'svg' | 'png'
+
+export interface IIconProps extends DOMAttributes<HTMLDivElement> {
+  className?: string
+  type: IconType
+  color?: string
+  size?: number
+}
---
+++ src/stories/UI/Components/Icon/styles/Icon.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+
+export const CustomIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`
